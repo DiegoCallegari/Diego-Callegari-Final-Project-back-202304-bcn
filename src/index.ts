82622bded76dcd7e385e1d2924fbed12c225dc24
diego callegari <callcallegari@gmail.com>
2023-05-25 14:47:15 +0200

    Add load environments and start server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export const a = 2;
+import "./loadEnvironment.js";
+import createdebug from "debug";
+import { app } from "./server/index.js";
+
+export const debug = createdebug("quefem-api:root");
+
+export const port = process.env.PORT ?? 4000;
+
+app.listen(port, () => {
+  debug(`listening on http://localhost:${port}`);
+});
