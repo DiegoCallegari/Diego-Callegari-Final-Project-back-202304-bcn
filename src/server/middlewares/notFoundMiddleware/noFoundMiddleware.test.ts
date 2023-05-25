fb541387dfdeb42c595a19b860345143e0f358d7
diego callegari <callcallegari@gmail.com>
2023-05-25 20:27:21 +0200

    Add notFoundMiddleware test

---
+++ src/server/middlewares/notFoundMiddleware/noFoundMiddleware.test.ts
@@ -0,0 +1,25 @@
+import { type NextFunction, type Request, type Response } from "express";
+import notFoundError from "./noFoundMiddleware.js";
+import { CustomError } from "../../../CustomError/CustomError";
+
+describe("Given a notFoundErrorMiddleware middleware", () => {
+  describe("When it is called", () => {
+    test("Then it should the next function with a status code 404 and the message 'Error not found' ", () => {
+      const customError = new CustomError(404, "Endpoint not found");
+
+      type CustomResponse = Pick<Response, "status" | "json">;
+
+      const res: CustomResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      const req = {};
+      const next = jest.fn();
+
+      notFoundError(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalledWith(customError);
+    });
+  });
+});
