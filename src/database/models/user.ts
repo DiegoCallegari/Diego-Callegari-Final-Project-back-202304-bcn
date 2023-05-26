662b1c545acf7586a75ec0b655c6ab79b0936b48
diego callegari <callcallegari@gmail.com>
2023-05-26 21:06:57 +0200

    Add userRouter test for valid user credentials case

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -14,7 +14,6 @@ const userSchema = new Schema({
 
   name: {
     type: String,
-    required: true,
   },
 
   surname: {
@@ -25,7 +24,6 @@ const userSchema = new Schema({
 
   email: {
     type: String,
-    required: true,
   },
 });
 
