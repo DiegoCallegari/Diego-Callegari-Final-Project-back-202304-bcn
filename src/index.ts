062e6925e22cb53ba479e959de0854cf5a22795f
diego callegari <callcallegari@gmail.com>
2023-05-26 10:22:41 +0200

    Connect mongoDb and add port listener

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,27 @@ import "./loadEnvironment.js";
 import createdebug from "debug";
 import { app } from "./server/index.js";
 import chalk from "chalk";
+import connectToDatabase from "./database/connectToDataBase.js";
 
 export const debug = createdebug("quefem-api:root");
 
-export const port = process.env.PORT ?? 4000;
+const port = process.env.PORT ?? 4000;
+
+const mongoDbConnection = process.env.MONGODB_CONNECTION;
+
+if (!mongoDbConnection) {
+  debug(chalk.red("Missing environmental variables. Exiting..."));
+  process.exit(1);
+}
 
 app.listen(port, () => {
   debug(chalk.blueBright(`listening on http://localhost:${port}`));
 });
+
+try {
+  await connectToDatabase(mongoDbConnection);
+
+  debug(chalk.blue("Connected to database"));
+} catch (error: unknown) {
+  debug(`Error connecting to database: ${chalk.red((error as Error).message)}`);
+}
