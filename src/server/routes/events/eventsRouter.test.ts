49b19006ccf077708442a404805cabd8a501bb16
diego callegari <callcallegari@gmail.com>
2023-06-10 23:24:07 +0200

    Add add endpoint test to create event

--- src/server/routes/events/eventsRouter.test.ts
+++ src/server/routes/events/eventsRouter.test.ts
@@ -9,8 +9,9 @@ import { eventsMock } from "../../../mocks/eventsMocks.js";
 import { app } from "../../app.js";
 
 let server: MongoMemoryServer;
+
 const tokenMock =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDcwODRiMjJhYTE1NTc4MjFhYmIwY2UiLCJuYW1lIjoiYWRtaW4iLCJpYXQiOjE2ODYzMDQ4NTUsImV4cCI6MTY4NjM5MTI1NX0.4unfgpRxGt98KkW9IXt-m4rZZSObYDTL_FnnPvSQgfo";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDcwODRiMjJhYTE1NTc4MjFhYmIwY2UiLCJuYW1lIjoiYWRtaW4iLCJpYXQiOjE2ODYzOTE5ODQsImV4cCI6MTY4NjQ3ODM4NH0.DeEC357UHw1VF8srOwrBKT-EwBs6QQBSCvYhNoJgx-Y";
 
 beforeAll(async () => {
   server = await MongoMemoryServer.create();
@@ -26,11 +27,15 @@ afterEach(async () => {
   await Event.deleteMany();
 });
 
-describe("Given a GET '/events' endpoint", () => {
-  beforeEach(async () => {
-    await Event.create(eventsMock);
-  });
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
+beforeEach(async () => {
+  await Event.create(eventsMock);
+});
+
+describe("Given a GET '/events' endpoint", () => {
   describe("When it receives a request with a valid token", () => {
     test("Then it should return a status code 200 and a list of events", async () => {
       const expectedStatusCode = 200;
@@ -46,10 +51,6 @@ describe("Given a GET '/events' endpoint", () => {
 });
 
 describe("Given a DELETE '/:id' endpoint", () => {
-  beforeEach(async () => {
-    await Event.create(eventsMock);
-  });
-
   describe("When it receives a request with a valid id", () => {
     test("Then it should return a status with status code '200' and the response's method json with 'Event removed' message", async () => {
       const expectedStatusCode = 200;
@@ -63,6 +64,30 @@ describe("Given a DELETE '/:id' endpoint", () => {
         .expect(expectedStatusCode);
 
       expect(response.body.message).toBe(expectedMessage);
+    });
+  });
+});
+
+describe("Given a POST '/add' endpoint", () => {
+  describe("When it recieve a request with an event and a new event data named 'Beach Concert'", () => {
+    test("Then it should return a statusCode 201 and the new event with name 'Beach Concert'", async () => {
+      const expectedStatusCode = 201;
+      const expectedProperty = "event";
+
+      const response = await request(app)
+        .post("/events/add")
+        .set("Authorization", `Bearer ${tokenMock}`)
+        .send({
+          title: "Beach",
+          image: "image",
+          neighbourhood: "Barceloneta",
+          description: "good vibes",
+          date: "2023, 6, 15",
+          category: "music",
+        })
+        .expect(expectedStatusCode);
+
+      expect(response.body).toHaveProperty(expectedProperty);
     });
   });
 });
